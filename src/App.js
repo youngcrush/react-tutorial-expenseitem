4959eb0eeac4707d85c5606f6b767c6ec4e2b6fd
Amit Choudhary <amit@18thdigitech.net>
2023-07-28 09:23:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+    { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Let's Get Started</h2>
+      <ExpenseItem
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+      ></ExpenseItem>
+      
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    return (
+        <div className='expense-item'>
+            <div>
+                <div>{month}</div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItem;
