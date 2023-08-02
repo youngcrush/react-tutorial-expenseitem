cc96868109ff619f5b00b890241818509ca6a484
Amit Choudhary <amit@18thdigitech.net>
2023-08-02 18:40:33 +0530

    add key

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+
+const INTIAL_EXPENSES = [
+  { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+  { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+  { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}
+];
+
 const App = () => {
-  const expenses = [
-    { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-    { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}
-  ];
+  const [expenses, setExpenses] = useState(INTIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
   // return React.createElement(
   //   'div',
